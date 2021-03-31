0d1f5183f12321ceeaadb1c4883855dc551b54ec
dlagosb <diegolagosbeltran@gmail.com>
2021-03-31 16:02:33 -0300

    node_modules

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import App from './App.svelte';
 import './firebase';
 import 'bootswatch/dist/lux/bootstrap.min.css';
 import 'toastr/build/toastr.js';
-import 'toastr/build/toastr.css';
+import 'toastr/build/toastr.min.css';
 
 const app = new App({
 	target: document.body,
